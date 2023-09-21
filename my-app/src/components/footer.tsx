3a9640ef62a9d12a6c7049a1ded32ee247546b8d
julianacamilo <julianacamilodebarros@gmail.com>
2023-09-21 00:57:00 -0300

    structure: create footer and header

---
+++ my-app/src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-purple-600 p-4 text-center text-white">
+      Todos os direitos reservados
+    </footer>
+  );
+};
+
+export default Footer;
