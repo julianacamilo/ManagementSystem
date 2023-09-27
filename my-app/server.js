769f79429179c10c3606a359175c7156315625d3
Gabriel Galvão dos Anjos <gabrieldosanjos136@gmail.com>
2023-09-26 22:29:15 -0300

    Update

--- my-app/server.js
+++ my-app/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const cors = require('cors'); // Adicione o pacote CORS para lidar com as requisições do cliente React.
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-app.use(cors()); // Habilita o CORS para aceitar solicitações do cliente React.
+app.use(cors());
 
 // Carrega o db.json
 const dbFile = 'db.json';
@@ -26,7 +26,6 @@ app.post('/register', (req, res) => {
     return res.status(400).json({ message: 'Email já cadastrado.' });
   }
 
-  // Cria um novo usuário
   const newUser = { name, email, password };
   db.users.push(newUser);
   fs.writeFileSync(dbFile, JSON.stringify(db, null, 2));
@@ -38,16 +37,121 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
-  // Verifica se o usuário existe
   const user = db.users.find((user) => user.email === email && user.password === password);
   if (!user) {
     return res.status(401).json({ message: 'Credenciais inválidas.' });
   }
 
-  // Gera um token JWT
   const token = jwt.sign({ email: user.email }, secretKey);
 
   res.status(200).json({ message: 'Login bem-sucedido.', token });
+});
+
+// Rota para exibir o relatório de despesas
+app.get('/relatorio-despesas', (req, res) => {
+  const despesasData = fs.readFileSync('despesas.json', 'utf8');
+  const despesas = JSON.parse(despesasData).despesas;
+
+  const totalDespesas = despesas.reduce((total, despesa) => total + despesa.valor, 0);
+
+  const html = `
+    <html>
+      <head>
+        <title>Relatório de Despesas</title>
+      </head>
+      <body>
+        <h1>Relatório de Despesas</h1>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Descrição</th>
+            <th>Valor</th>
+          </tr>
+          ${despesas.map(despesa => `
+            <tr>
+              <td>${despesa.id}</td>
+              <td>${despesa.descricao}</td>
+              <td>${despesa.valor}</td>
+            </tr>
+          `).join('')}
+        </table>
+        <p>Total de Despesas: R$ ${totalDespesas.toFixed(2)}</p>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
+// Rota para exibir o relatório de ganhos
+app.get('/relatorio-ganhos', (req, res) => {
+  const ganhosData = fs.readFileSync('ganhos.json', 'utf8');
+  const ganhos = JSON.parse(ganhosData).ganhos;
+
+  const totalGanhos = ganhos.reduce((total, ganho) => total + ganho.valor, 0);
+
+  const html = `
+    <html>
+      <head>
+        <title>Relatório de Ganhos</title>
+      </head>
+      <body>
+        <h1>Relatório de Ganhos</h1>
+        <table>
+          <tr>
+            <th>Data</th>
+            <th>Descrição</th>
+            <th>Valor</th>
+          </tr>
+          ${ganhos.map(ganho => `
+            <tr>
+              <td>${ganho.data}</td>
+              <td>${ganho.descricao}</td>
+              <td>${ganho.valor}</td>
+            </tr>
+          `).join('')}
+        </table>
+        <p>Total de Ganhos: R$ ${totalGanhos.toFixed(2)}</p>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
+// Rota para exibir o relatório de metas
+app.get('/relatorio-metas', (req, res) => {
+  const metasData = fs.readFileSync('metas.json', 'utf8');
+  const metas = JSON.parse(metasData).metas;
+
+  const html = `
+    <html>
+      <head>
+        <title>Relatório de Metas</title>
+      </head>
+      <body>
+        <h1>Relatório de Metas</h1>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Descrição</th>
+            <th>Valor</th>
+            <th>Progresso</th>
+          </tr>
+          ${metas.map(meta => `
+            <tr>
+              <td>${meta.id}</td>
+              <td>${meta.descricao}</td>
+              <td>${meta.valor}</td>
+              <td>${meta.progresso}</td>
+            </tr>
+          `).join('')}
+        </table>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 // Inicie o servidor
