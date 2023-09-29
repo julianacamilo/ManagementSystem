d66d41400ae7da51973739988b3bad5ebbe17536
julianacamilo <julianacamilodebarros@gmail.com>
2023-09-28 23:13:24 -0300

    create: responsiveness page ganhos

--- my-app/pages/ganhos.tsx
+++ my-app/pages/ganhos.tsx
@@ -29,39 +29,39 @@ const Ganhos = () => {
 
   return (
     <div>
-      <Header />
-      <div
-        className="bg-gray-100 min-h-screen flex flex-col justify-center items-center"
-        style={{
-          backgroundImage: `url(https://roxceramica.com.br/uploads/produtos/imagens//RXAR%2072732%20-%20CINZA%20REAL%20ACETINADO%20-BAIXA.jpg')`, // 
-          backgroundSize: 'cover',
-          backgroundPosition: 'center', 
-        }}
-      >
-        <div className="container mx-auto flex justify-center items-center h-screen">
-          <div className="flex space-x-5">
-            <div className="w-1/2">
-              <div className="max-w-screen-sm mx-auto bg-white-200 p-6 rounded-md shadow-md mt-4 w-full">
-                <h1 className="text-1xl text-purple-600 font-semibold mb-4">
-                  Adicione seus ganhos mensais <br /> e assim gerencie seu financeiro!
-                </h1>
-                <div
-                  className="image-and-text"
-                  style={{
-                    backgroundImage: `url('https://img.freepik.com/fotos-premium/dinheiro-crescimento-negocio-grafico-economia-financas-estoque-ou-moeda-investimento-diagrama-financeiro-grafico-de-lucro-de-acoes-de-precos-de-mercado-de-cambio-global-na-analise-economica-fundo-3d-com-comercio-de-sucesso_79161-2035.jpg')`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    height: '320px',
-                  }}
-                >
-                  <p></p>
-                </div>
+    <Header />
+    <div
+      className="bg-gray-100 min-h-screen flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `url(https://roxceramica.com.br/uploads/produtos/imagens//RXAR%2072732%20-%20CINZA%20REAL%20ACETINADO%20-BAIXA.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row">
+          <div className="md:w-1/2 flex-grow"> 
+            <div className="max-w-screen-sm mx-auto bg-white p-6 rounded-md shadow-md mt-4 w-full">
+              <h1 className="text-1xl text-purple-600 font-semibold mb-4">
+                Adicione seus ganhos mensais e assim gerencie seu financeiro!
+              </h1>
+              <div
+                className="image-and-text"
+                style={{
+                  backgroundImage: `url('https://img.freepik.com/vetores-premium/mulher-ilustrada-com-graficos-de-analise-do-mercado-de-acoes_23-2148619248.jpg?w=2000')`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  height: '290px',
+                }}
+              >
+                <p></p>
               </div>
             </div>
-            <div className="w-1/2">
-              <div className="max-w-screen-sm mx-auto flex flex-col items-center">
-                <div className="max-w-md mx-auto  bg-white-400 p-6 rounded-md shadow-md mt-4 w-full">
-                  <h1 className="text-2xl text-purple-600 font-semibold mb-4">Renda <br /> Mensal </h1>
+          </div>
+          <div className="md:w-1/2 flex-grow"> 
+            <div className="max-w-screen-sm mx-auto flex flex-col items-center">
+              <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md mt-4 w-full">
+                <h1 className="text-2xl text-purple-600 font-semibold mb-4">Renda Mensal</h1>
 
                   <div className="mb-4">
                     <label htmlFor="description" className="block text-purple-600 font-bold mb-1">
