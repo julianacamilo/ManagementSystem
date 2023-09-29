80c103846b383170e8be041e21bfa2cec8b7a39c
Leonardo Nadolny Magalhaes <leonardonmagalhaes@gmail.com>
2023-09-29 11:50:59 -0300

    Organização de imports na pag de login

--- my-app/pages/login.tsx
+++ my-app/pages/login.tsx
@@ -1,7 +1,9 @@
 import Footer from '@/components/footer';
 import Header from '@/components/head';
+
 import React, { useState } from 'react';
 import { useRouter } from 'next/router'; 
+
 import 'tailwindcss/tailwind.css';
 import axios from 'axios';
 
