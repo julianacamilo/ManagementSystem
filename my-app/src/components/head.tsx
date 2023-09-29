cf3fa8368acf19d56a12fb9a4966a70b52d8c5da
claudio souza <claudiohenrique458@gmail.com>
2023-09-29 01:06:37 -0300

    fiz as mudancas na header

--- my-app/src/components/head.tsx
+++ my-app/src/components/head.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
+
 const Header: React.FC = () => {
   return (
     <header className="bg-purple-600 p-4 flex justify-between items-center text-white">
-    <div className="text-2xl font-bold">Finanças</div>
-    <div className="flex items-center">
-      <div className="ml-4">Saiba mais sobre Finanças</div>
-      <div className="ml-4">Centro de Segurança</div>
-    
-    </div>
-  </header>
+      <div className="text-2xl font-bold lg:flex hidden">Finanças</div> {/* Exibir em telas maiores */}
+      <div className="text-2xl font-bold flex justify-center lg:hidden">Finanças</div> {/* Centralizar em telas menores */}
+      <div className="flex items-center lg:flex hidden"> {/* Exibir em telas maiores */}
+        <div className="ml-4">Saiba mais sobre Finanças</div>
+        <div className="ml-4">Centro de Segurança</div>
+      </div>
+    </header>
   );
 };
 
