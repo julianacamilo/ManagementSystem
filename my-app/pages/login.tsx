97271fe9a4f28f25f75b0eee7ddec94c32a50fe7
Leonardo Nadolny Magalhaes <leonardonmagalhaes@gmail.com>
2023-09-29 11:55:14 -0300

    Alterar URL da solicitação Axios para API de login
    Atualizar a URL da solicitação Axios para corresponder à configuração do servidor adequado.

--- my-app/pages/login.tsx
+++ my-app/pages/login.tsx
@@ -29,7 +29,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:3001/login', {
+      const response = await axios.post('http://localhost:3003/login', {
         email,
         password,
       });
